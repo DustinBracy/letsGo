e5f18e85bab5555af1d09b03e3db7eda5dd8d3dd
DustinBracy <dustin@bracytechnology.com>
2025-05-08 22:39:42 -0500

    book 2 start of ch5

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -6,12 +6,40 @@ import (
 	"time"
 
 	"github.com/DustinBracy/letsGo/greenlight/internal/data"
+	"github.com/DustinBracy/letsGo/greenlight/internal/validator"
 )
 
-// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
-// return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	// Call the ValidateMovie() function, and if any checks fail, return a response
+	// containing the errors.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
