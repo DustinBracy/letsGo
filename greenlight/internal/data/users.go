649bca2571f7d91bf18bca5ee49144b9df4a2e75
DustinBracy <dustin@bracytechnology.com>
2025-05-17 17:23:35 -0500

    book 2 start of ch16

--- greenlight/internal/data/users.go
+++ greenlight/internal/data/users.go
@@ -11,10 +11,9 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// Define a User struct to represent an individual user. Importantly, notice how we are
-// using the json:"-" struct tag to prevent the Password and Version fields from appearing
-// in any output when we encode it to JSON. Also notice that the Password field uses the
-// custom password type defined below.
+// Declare a new AnonymousUser variable.
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -23,6 +22,11 @@ type User struct {
 	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
+}
+
+// Check if a User instance is the AnonymousUser.
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 // Create a custom password type which is a struct containing the plaintext and hashed
